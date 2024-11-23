63da0d9e3d5d066e3ab8f75fe32ec2178d0a6d6e
vladmaryna <vlad.maryna.clg@chnu.edu.ua>
2024-11-23 22:25:07 +0200

    Add files via upload

---
+++ lab_9_1(a).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   for(int i=20; i<=35 ; i++){
+       
+       if(i%2==0){
+           
+           cout << i<< endl;
+       }
+   }
+    return 0;
+}
