48456ea2f6296514ef77803f498f520be274f61d
vladmaryna <vlad.maryna.clg@chnu.edu.ua>
2024-11-26 12:06:22 +0200

    Add files via upload

---
+++ lab_9_3(a).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double sum=0.0
+    for (int i=1; i<=20; i++){
+        for (int j=1; j<=30; j++){
+         sum=sum+i/j   
+        }
+        
+    }
+    cout << sum << endl;
+    
+    return 0;
+}
